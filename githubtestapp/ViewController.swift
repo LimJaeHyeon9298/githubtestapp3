df33a734fdbf387186da01a20343f9d396651bee
JaeHyun <limjh9298@gmail.com>
2022-11-20 14:03:15 +0900

    Initial Commit

---
+++ githubtestapp/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  githubtestapp
+//
+//  Created by 임재현 on 2022/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    configeUI()
+        
+        
+    }
+    
+   func configeUI(){
+        
+        imageView.image = #imageLiteral(resourceName: "black1")
+    }
+    
+    
+    
+}
+
